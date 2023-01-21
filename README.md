91199679be42874ad27497079f390c3666581644
Thiago Silva <thiagowfer@gmail.com>
2023-01-21 00:00:40 -0300

    mvp ready to deploy

---
+++ README.md
@@ -0,0 +1,15 @@
+## Best Eats
+
+### A Website about foods
+
+- Made in Reactjs 
+- Styled with TailwindCSS
+- Fully Responsive
+- Hover animations
+- Filter functions
+
+### 👁️[ View Online](https://frontiago-best-eats.vercel.app/)
+
+---
+
+![Print](./src/assets/print.png)
---
+++ src/components/Category.jsx
@@ -0,0 +1,21 @@
+import {categories} from '../data/data'
+
+const Category = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto px-6 py-12">
+        <h1 className="mb-6 text-orange-600 font-bold text-4xl text-center">Top Rated Menu Categories</h1>
+
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">   
+            {categories.map((item, index) => (
+                <div className="bg-gray-100 rounded-lg p-4 flex justify-between items-center cursor-pointer hover:scale-105 duration-500" key={index}>
+                    <h2 className="font-bold sm:text-xl">{item.name}</h2>
+                    <img className="" src={item.image} alt={item.name} />
+                </div>
+            ))}
+           
+        </div>
+    </div>
+  )
+}
+
+export default Category
