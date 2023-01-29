bf1a29f528643668f507119e3e2205e5a986617b
Thiago Silva <thiagowfer@gmail.com>
2023-01-29 20:47:40 -0300

    add detail property to the food data file

--- src/data/data.js
+++ src/data/data.js
@@ -6,6 +6,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
       price: 11.99,
+      detail: 'A delicious Bacon Chesseburger full of Cheddar'
     },
     {
       id: 2,
@@ -14,6 +15,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
       price: 22.85,
+      detail: 'Amazing Mushroom Burger that will give to you all strenght you need'
     },
     {
       id: 3,
@@ -22,6 +24,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
       price: 30.99,
+      detail: 'Double meat and salad, this is the loaded burger'
     },
     {
       id: 4,
@@ -38,6 +41,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1604382355076-af4b0eb60143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
       price: 33.80,
+      detail: 'So taste that even Popeye is gonna buy'
     },
     {
       id: 6,
@@ -46,6 +50,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
       price: 40.99,
+      detail: 'Great flavor and full of pepperoni'
     },
     {
       id: 7,
@@ -54,6 +59,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1548369937-47519962c11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
       price: 10.00,
+      detail: "Are you a cheese lover? so this pizza it's for you"
     },
     {
       id: 8,
@@ -62,6 +68,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1515543237350-b3eea1ec8082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
       price: 20.00,
+      detail: 'Need something light? The Kale Salad is gonna do the job'
     },
     {
       id: 9,
@@ -70,6 +77,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2FsYWQlMjBjZWFzYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
       price: 34.99,
+      detail: "It's not from Rome but has an army of salads"
     },
     {
       id: 10,
@@ -78,6 +86,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1540420773420-3366772f4999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FsYWR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
       price: 41.85,
+      detail: 'The Loaded Salad has is the perfect mix between fit and light'
     },
     {
       id: 11,
@@ -86,6 +95,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
       price: 15.99,
+      detail: 'If you are a fruit lover, you gonna love this even more'
     },
     {
       id: 12,
@@ -94,6 +104,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
       price: 23.50,
+      detail: 'A delicious chicken wings that will make your stomach happy'
     },
     {
       id: 13,
@@ -102,6 +113,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
       price: 40.50,
+      detail: 'A giant Chicken that will feed an entire family'
     },
     {
       id: 14,
@@ -110,6 +122,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
       price: 11.99,
+      detail: 'This is the perfect snack to begin the night beside someone you love'
     },
     {
       id: 15,
@@ -118,6 +131,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
       price: 35.50,
+      detail: 'A snack that will make your group happy'
     },
     {
       id: 16,
@@ -126,6 +140,7 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
       price: 40.00,
+      detail: 'A burger so big that will double your happiness'
     },
   ];
   
